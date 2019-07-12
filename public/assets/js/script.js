fe9d45cfb5ff9e65a023cff1f6126cda92a6c52a
Yadira Soto <lunar13heart@hotmail.com>
2019-07-11 22:36:50 -0400

    edited script

---
+++ public/assets/js/script.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevour = $(this).data("newdevour");
+  
+      var newDevourState = {
+        devour: newDevour
+      };
+  
+   
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed devour to", newDevour);
+        
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#bg").val().trim(),
+        devour: $("[name=devour]:checked").val().trim()
+      };
+  
+      .
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+      
+          location.reload();
+        }
+      );
+    });
+  });
+  
