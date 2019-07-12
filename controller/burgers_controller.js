024f6f39c631b9f1f389bed4471e9313db12ce5f
Yadira Soto <lunar13heart@hotmail.com>
2019-07-11 21:43:23 -0400

    edited this file

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -1,10 +1,63 @@
-var express = require("express");
-
-var router = express.Router();
-
-
 // look at cats activity
 // router.get
 // router.post
 
+var express = require("express");
+var router = express.Router();
+
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      cats: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burger", function(req, res) {
+  burger.create([
+    "Burger a la Mode", "Summer Thyme Burger"
+  ], [
+    req.body.name, req.body.summer
+  ], function(result) {
+    
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    summer: req.body.summer
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
 module.exports = router;
