101013df617dfe3dfab061d5016b01763252d7d5
Yadira Soto <lunar13heart@hotmail.com>
2019-07-11 22:32:30 -0400

    updated burger.js

--- db/models/burger.js
+++ db/models/burger.js
@@ -1,15 +1,46 @@
+// var orm = require("../config/orm.js");
+
+// // cats.js
+
+// var burger = {
+//     selectall
+//     orm.selectall
+
+//     insertOne
+//     orm.insertOne
+
+//     updateOne
+//     orm.updateOne
+// }
+// model.
+
+
+// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-// cats.js
-
 var burger = {
-    selectall
-    orm.selectall
+  all: function(bg) {
+    orm.all("burgers", function(res) {
+      bg(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, bg) {
+    orm.create("burgers", cols, vals, function(res) {
+      bg(res);
+    });
+  },
+  update: function(objColVals, condition, bg) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      bg(res);
+    });
+  },
+  delete: function(condition, bg) {
+    orm.delete("burgers", condition, function(res) {
+      bg(res);
+    });
+  }
+};
 
-    insertOne
-    orm.insertOne
-
-    updateOne
-    orm.updateOne
-}
-model.
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
