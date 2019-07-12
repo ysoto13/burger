54fbc0eed73448d54bc71cc934165041f0b813d0
Yadira Soto <lunar13heart@hotmail.com>
2019-07-11 22:12:51 -0400

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,12 @@
 CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
-DROP TABLE IF EXISTS  burgers;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devour BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
 
