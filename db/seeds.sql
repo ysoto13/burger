24dcfc5a146628a585ce9c778969c710b0176d6b
Yadira Soto <lunar13heart@hotmail.com>
2019-07-11 22:11:47 -0400

    added table information to this file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,11 @@
 -- information to put into your database
 
-INSERT INTO burgers(burgername devour) valuse(cheeseburger, false)
+-- INSERT INTO burgers(burgername devour) valuse(cheeseburger, false)
 
 
-take sql from CATS acitivity from class of 7/9/19 to make it easier/faster
+INSERT INTO burgers (name) VALUES ("Burger a la Mode");
+INSERT INTO burgers (name) VALUES ("Summer Thyme Burger");
+INSERT INTO burgers (name, devour) VALUES ("Beets Me Burger", false);
+INSERT INTO burgers (name, devour) VALUES ("Let It Snow Peas Burger", false);
+INSERT INTO burgers (name, devour) VALUES ("Girls Just Wanna Have Fennel Burger", false);
+INSERT INTO burgers (name) VALUES ("Chorizo Your Own Adventure Burger");
